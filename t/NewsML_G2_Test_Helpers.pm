f45839544917084efe5b310e6726d7f2649ce7f5
Philipp Gortan <gortan@cpan.org>
2014-10-17 07:49:17 +0000

    Import of GORTAN/XML-NewsML_G2-v0.1_4 from CPAN.
    gitpan-cpan-distribution: XML-NewsML_G2
    gitpan-cpan-version:      v0.1_4
    gitpan-cpan-path:         GORTAN/XML-NewsML_G2-v0.1_4.tar.gz
    gitpan-cpan-author:       GORTAN
    gitpan-cpan-maturity:     developer

--- t/NewsML_G2_Test_Helpers.pm
+++ t/NewsML_G2_Test_Helpers.pm
@@ -1,6 +1,6 @@
 package NewsML_G2_Test_Helpers;
 
-# $Id: NewsML_G2_Test_Helpers.pm 57048 2014-10-15 15:31:35Z apatechrdlicka $
+# $Id: NewsML_G2_Test_Helpers.pm 57138 2014-10-17 07:21:20Z apatecgortan $
 
 use Exporter 'import';
 use File::Spec::Functions qw(catfile);
@@ -113,15 +113,19 @@ our @keywords = qw(beer vienna prater kolarik schweizerhaus);
 
 sub validate_g2 {
     my ($dom, $version) = @_;
-    $version =~ s/\./_/;
 
-    my $xsd = catfile('t', 'xsds', "NewsML-G2_$version-spec-All-Power.xsd");
-    ok(my $xmlschema = XML::LibXML::Schema->new(location => $xsd), "parsing $version XSD");
+  SKIP: {
+        skip 'libxml2 before 2.8 reports bogus violation on children of "broader"', 2 if (20800 > XML::LibXML::LIBXML_RUNTIME_VERSION);
+        $version =~ tr/./_/;
+        my $xsd = catfile('t', 'xsds', "NewsML-G2_$version-spec-All-Power.xsd");
+        ok(my $xmlschema = XML::LibXML::Schema->new(location => $xsd), "parsing $version XSD");
 
-    lives_ok(sub {$xmlschema->validate($dom)}, "XML validates against $version XSD");
+        lives_ok(sub {$xmlschema->validate($dom)}, "XML validates against $version XSD");
+    }
 
     return;
 }
+
 
 sub _create_ni {
     my $ni_cls = shift;
